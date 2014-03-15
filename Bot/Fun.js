888c79b6043ed0eb58d62724f3856f0741bd6465
wildebevst <wrevans95@yahoo.com>
2014-03-15 15:06:57 -0500

    Update Fun.js

--- Bot/Fun.js
+++ Bot/Fun.js
@@ -85,8 +85,7 @@ var blockedSongs = [
     "Rick Roll",
     "GANGNAM",
     "The Fox",
-    "I got Bitches"
-    "The Fox [Official music video HD]",
+    "I got Bitches",
     "10 hour",
     "Trololo"
 ];
